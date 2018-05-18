42d30afb89d3db5f3a0f734da63ce6ae6e2c3c27
Kenny Do <chinesedewey@gmail.com>
2018-05-18 00:33:54 -0700

    Revert "intentionally bad commit"
    This reverts commit 6fd9f906aa9b1d01245ddae9f04fdd64986e9731.

--- cmd/artesia/app/config.go
+++ cmd/artesia/app/config.go
@@ -5,6 +5,7 @@ type Config struct {
 	ListenAddress string
 }
 
+// NewDefaultConfig returns a default config
 func NewDefaultConfig() *Config {
 	return &Config{
 		ListenAddress: "127.0.0.1:8080",
