6fd9f906aa9b1d01245ddae9f04fdd64986e9731
Kenny Do <chinesedewey@gmail.com>
2018-05-18 00:33:34 -0700

    intentionally bad commit

--- cmd/artesia/app/config.go
+++ cmd/artesia/app/config.go
@@ -5,7 +5,6 @@ type Config struct {
 	ListenAddress string
 }
 
-// NewDefaultConfig returns a default config
 func NewDefaultConfig() *Config {
 	return &Config{
 		ListenAddress: "127.0.0.1:8080",
